f0ed4824e88c96b13e9b78e6f55879af3cbed4a0
poojithareddy19 <158087326+poojithareddy19@users.noreply.github.com>
2024-01-30 00:08:02 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,16 @@
+str=('BMW','Audi','Jaguar','BMW')
+for i in str:
+    print(i)
+
+
+
+
+cls=('name','sec','rollno')
+for x in cls:
+    print(x)
+
+
+
+list=['name',15,10.5]
+for j in list:
+    print(j)
